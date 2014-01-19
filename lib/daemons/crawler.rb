09779ff5dcb608912e7fa1b4d11e48999e9d16e6
Stephen Bussey <sb8244@cs.ship.edu>
2014-01-18 21:49:49 -0500

    Search functionality, view vamps, crawler error catching

--- lib/daemons/crawler.rb
+++ lib/daemons/crawler.rb
@@ -19,41 +19,46 @@ while($running) do
   #could also be parallelized, but this is a hackathon and i'm lazy
   watches = Watch.all 
   watches.each do |watch|
-    #Check to see if 2 minutes have passed since the last check
-    if(watch.history.last.nil? || watch.history.last.updated_at - (Time.now - 2.minutes) < 0 )
-      uri = URI(watch.url)
-      #Grab a hash of the page and compare it to the old hash
-      new_page_hash = Digest::MD5.hexdigest( Net::HTTP.get( uri ) )
+    begin
+      #Check to see if 2 minutes have passed since the last check
+      if(watch.history.last.nil? || watch.history.last.updated_at - (Time.now - 2.minutes) < 0 )
+        uri = URI(watch.url)
+        #Grab a hash of the page and compare it to the old hash
+        new_page_hash = Digest::MD5.hexdigest( Net::HTTP.get( uri ) )
 
-      #trigger a job if the last history is null or the page content has changed
-      trigger_job = watch.history.last.nil? || new_page_hash != watch.history.last.page_hash
-      if trigger_job 
-        #This service will take an image of the content for the very specific selector we have
-        result = open("http://content2img.com:4000?trim=30&url=#{ERB::Util::url_encode(watch.url)}&selector=#{ERB::Util::url_encode(watch.selector)}").read
-        json = JSON.parse(result)
-        last_content = watch.history.last ? watch.history.last.content : ""
-        #process this if there is an error and the last content was not an error
-        process = !json['error'].nil? && last_content != "not found"
-        #process this if the content is not null and does not equal the last content
-        process = process || (!json['content'].nil? && json['content']['html'] != last_content)
-        puts process
-        if process 
-          history = History.new do |h|
-            h.content = json['content']['html'] unless json['error'] == 'not found'
-            h.content = "not found" if json['error'] == 'not found'
-            h.page_hash = new_page_hash
-            h.watch = watch
-            h.image_id = json['id']
-            h.save
+        #trigger a job if the last history is null or the page content has changed
+        trigger_job = watch.history.last.nil? || new_page_hash != watch.history.last.page_hash
+        if trigger_job 
+          #This service will take an image of the content for the very specific selector we have
+          #should probably rescue read / timeout errors instead of throwing
+          result = open("http://content2img.com:4000?trim=30&url=#{ERB::Util::url_encode(watch.url)}&selector=#{ERB::Util::url_encode(watch.selector)}",
+                        'r', :read_timeout=>30).read
+          json = JSON.parse(result)
+          last_content = watch.history.last ? watch.history.last.content : ""
+          #process this if there is an error and the last content was not an error
+          process = !json['error'].nil? && last_content != "not found"
+          #process this if the content is not null and does not equal the last content
+          process = process || (!json['content'].nil? && json['content']['html'] != last_content)
+          if process 
+            history = History.new do |h|
+              h.content = json['content']['html'] unless json['error'] == 'not found'
+              h.content = "not found" if json['error'] == 'not found'
+              h.page_hash = new_page_hash
+              h.watch = watch
+              h.image_id = json['id']
+              h.save
+            end
+            #ChangeMailer.notify_user(watch.user, history).deliver
+            puts "Job processed & Notified: #{watch.url}::#{watch.selector}"
+          else
+            last = watch.history.last
+            last.updated_at = DateTime.now
+            last.save
           end
-          #ChangeMailer.notify_user(watch.user, history).deliver
-          puts "Job processed & Notified: #{watch.url}::#{watch.selector}"
-        else
-          last = watch.history.last
-          last.updated_at = DateTime.now
-          last.save
         end
-      end
-    end
+      end  
+    rescue Exception => e
+      puts "Exception caught: #{e.message}"
+    end  
   end
 end
